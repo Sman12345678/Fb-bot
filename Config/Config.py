e18ec207e3ab7a25846c28323621e0cdba550b65
Suleiman <Suleimanayomide716@gmail.com>
2025-01-21 08:15:12 +0100

    Delete Config directory

--- Config/Config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    PREFIX = os.getenv("PREFIX")  # The prefix for commands
-    BOT_ADMIN = os.getenv("BOT_ADMIN")  # Bot admin's ID
